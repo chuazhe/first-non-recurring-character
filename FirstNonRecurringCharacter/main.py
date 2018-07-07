476976dfc1487992b4b0b09ef5839e3f4ed2373c
chuazhe <38622580+chuazhe@users.noreply.github.com>
2018-07-07 21:15:57 +0800

    Changes

--- FirstNonRecurringCharacter/main.py
+++ FirstNonRecurringCharacter/main.py
@@ -1,18 +1,26 @@
 inputString=input("Please enter the string:")
+resultASCIICode=-1
 
-charList=[]
-for i,char in enumerate(inputString):
-    if char in charList:
-        charList.remove(char)
-    else:
-        charList.append(char)
+# initialise the count of the array of size 256 to 0
+# because the character is 0-255 only
+count=[0]*256
 
-if not charList:
+#Start and calculate the count of character of the string
+for char in inputString:
+    # ord: Cnvert the character to its ascii code, for example a->97
+    count[ord(char)]+=1
+
+# Start finding the first non recurring character
+for char in inputString:
+    if count[ord(char)] == 1:
+        resultASCIICode=ord(char)
+        break
+
+if resultASCIICode == -1:
     print("No non reccuring character!")
 else:
-    print("The first non recurring character is "+charList[0])
-
-
+    # chr: Convert the ascii code to character
+    print("The first non recurring character is "+chr(resultASCIICode))
 
 
 
